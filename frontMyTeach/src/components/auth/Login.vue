732ebacc81bfb9c0d6330e43789a3caf21f7bb79
zerovel666 <zerovel060606@gmail.com>
2025-02-13 11:40:24 +0500

    fix

--- frontMyTeach/src/components/auth/Login.vue
+++ frontMyTeach/src/components/auth/Login.vue
@@ -3,11 +3,13 @@
 		<PreLoad :show-logo="showLogo" />
 		<transition name="fade">
 			<div v-if="!showLogo" class="loginCont">
+				<img src="/src/assets/Icons/Icon.svg" alt="" id="logo">
 				<form action="" class="loginForm">
 					<input type="text" placeholder="Login">
 					<input type="text" placeholder="Password">
 					<button>Auth</button>
 				</form>
+				<a href="">Don't have account?</a>
 			</div>
 		</transition>
 	</div>
@@ -22,12 +24,17 @@ const showLogo = ref(true);
 onMounted(() => {
 	setTimeout(() => {
 		showLogo.value = false;
-	}, 3000);
+	}, 1000);
 });
 </script>
 
 <style scoped>
-
+#logo{
+	position: absolute;
+	top: -90%;
+	left: 35%;
+	width: 100px;
+}
 .container {
 	display: flex;
 	justify-content: center;
@@ -38,22 +45,23 @@ onMounted(() => {
 
 .loginCont {
 	width: 300px;
-	height: 300px;
 	transition: opacity 1s ease-in-out;
 	position: absolute;
+	flex-wrap: wrap;
+	justify-content: center;
+	align-items: center;
 }
 .loginForm {
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
-	height: 100%;
 }
 
 .loginForm input {
-    width: 280px;
-    height: 40px;
-    margin: 15px;
+    width: 240px;
+    height: 30px;
+    margin: 10px;
     border-radius: 15px;
     display: flex;
     align-items: center; 
@@ -61,14 +69,29 @@ onMounted(() => {
     font-size: 24px;
 }
 
-.loginForm button{
+.loginForm button {
 	width: 140px;
-	height: 40px;
+	height: 30px;
 	border-radius: 15px;
-	font-size: 32px;
+	font-size: 24px;
 	font-weight: 100;
 	box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.5);
+	margin:10px;
 }
+
+@import url('https://fonts.googleapis.com/css2?family=Anonymous+Pro:wght@400&display=swap');
+
+.loginCont a {
+	display: flex;
+	justify-content: center;
+	text-align: center;
+	text-decoration: none;
+	color: white;
+	font-size: 14px;
+	margin-top: 10%;
+	font-family: 'Anonymous Pro', monospace;
+}
+
 
 .fade-enter-active,
 .fade-leave-active {
