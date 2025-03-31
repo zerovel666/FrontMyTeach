4248ed6fcbd835e7540bc885d227bd8e48c91703
zerovel666 <zerovel060606@gmail.com>
2025-03-31 18:52:58 +0500

    add analytics

--- frontMyTeach/src/App.vue
+++ frontMyTeach/src/App.vue
@@ -6,10 +6,12 @@
 </template>
 
 <script setup>
-import { watch } from 'vue';
+import { watch, onMounted, onBeforeUnmount } from 'vue';
 import { useRouter } from 'vue-router';
 import { getCurrentInstance } from 'vue';
 import VueCookies from 'vue-cookies';
+import axios from 'axios';
+import { API_URL } from './config';
 
 const router = useRouter();
 const instance = getCurrentInstance();
@@ -25,8 +27,33 @@ watch(
 if (!VueCookies.get('token')) {
 	router.push('/auth');
 }
+
+let startTime = 0;
+
+onMounted(() => {
+	if (VueCookies.get('token')) {
+		startTime = Date.now();
+	}
+});
+
+const logTimeSpent = async () => {
+	if (VueCookies.get('token')) {
+		const totalTime = Math.floor((Date.now() - startTime) / 1000);
+		const data = {
+			time: totalTime
+		};
+		console.log('goAwait');
+		await axios.post(`${API_URL}/client/analytics/time`, data)
+	}
+};
+
+onBeforeUnmount(logTimeSpent);
+window.addEventListener('beforeunload', logTimeSpent);
+
+onBeforeUnmount(() => {
+	window.removeEventListener('beforeunload', logTimeSpent);
+});
 </script>
 
-<style scoped>
 
-</style>
+<style scoped></style>
